6b39ac632174611d4d036276084ca0b9360791f1
nam712 <namchetql169@gmail.com>
2024-09-18 21:21:46 +0700

    bt1

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+        source={require('./assets/logo-CMC-University-3-400x71.png')} 
+        style={styles.image} 
+        resizeMode="contain"  
+      />
+            <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200, 
+    height: 50, 
+    marginTop: 20, 
   },
 });
